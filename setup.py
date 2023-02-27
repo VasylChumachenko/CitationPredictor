688ca4aeb255d1af3fc2a2909aeda5722f158b9c
Vasyl <chumachenko_va@ukr.net>
2023-02-27 18:36:11 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='citation_predictor',
+    version='0.0.1',    
+    description='POC',
+    url='https://github.com/shuds13/pyexample',
+    author='Vasyl Chumachenko',
+    author_email='chumachenko_va@ukr.net',    
+    license='BSD 2-clause',
+    packages=['citation_predictor'],
+    install_requires=['pandas',
+                      'numpy',
+                      ''],
+
+    classifiers=[
+        'Development Status :: 1 - alpha',
+        'Intended Audience :: Science/Research',,
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)   
